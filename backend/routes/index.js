c9d60c1b8490239bd06caacb45f5a67b21a0e841
Song Yang <keesongyang4@gmail.com>
2023-05-15 00:38:56 +0800

    Install Socket.io

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+// module.exports = router;
+
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server, {cors: {origin: "*"}})
+
+app.get('/home', (req, res) => {
+  res.render('home')
+})
+
+server.listen(3001, () => {
+  console.log('Sever running');
 });
-
-module.exports = router;
