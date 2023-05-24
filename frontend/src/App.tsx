1ed85403a58d13435348dea44f7b503ec1a85a58
raynertjx <rayner742@gmail.com>
2023-05-25 00:29:13 +0800

    Added react router logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Layout from './pages/Layout';
-import { TypingTest } from './pages/TypingTest';
+import Layout from './routes/Layout';
+import Login from './routes/Login';
+import { TypingTest } from './routes/TypingTest';
 
 function App() {
   return (
-    <Layout>
-      <TypingTest />
-    </Layout>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<TypingTest />} />
+        <Route path="login" element={<Login />} />
+      </Route>
+    </Routes>
   );
 }
 
