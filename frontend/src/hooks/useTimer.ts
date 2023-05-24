ce914fe02cabf9b1a5ee09f112ebe8e580163008
raynertjx <rayner742@gmail.com>
2023-05-24 20:16:38 +0800

    Added useTimer hook

---
+++ frontend/src/hooks/useTimer.ts
@@ -0,0 +1,44 @@
+import { useEffect, useRef, useState } from 'react';
+
+interface TimerControls {
+  startTimer: () => void;
+  pauseTimer: () => void;
+  resetTimer: () => void;
+}
+
+const useTimer = (initialTime: number): [number, TimerControls] => {
+  const [time, setTime] = useState(initialTime);
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = useRef<number | null>(null);
+
+  const startTimer = () => {
+    setIsRunning(true);
+  };
+
+  const pauseTimer = () => {
+    setIsRunning(false);
+  };
+
+  const resetTimer = () => {
+    setIsRunning(false);
+    setTime(initialTime);
+  };
+
+  useEffect(() => {
+    if (isRunning) {
+      intervalRef.current = window.setInterval(() => {
+        setTime((prevTime) => prevTime - 1);
+      }, 1000);
+    } else {
+      clearInterval(intervalRef.current!);
+    }
+
+    return () => {
+      clearInterval(intervalRef.current!);
+    };
+  }, [isRunning]);
+
+  return [time, { startTimer, pauseTimer, resetTimer }];
+};
+
+export default useTimer;
