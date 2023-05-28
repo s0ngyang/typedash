3603d7f9be0da4e2b0de55723d6d4103d8460ff0
raynertjx <rayner742@gmail.com>
2023-05-28 22:50:49 +0800

    Added basic auth with register login logout functionality

--- frontend/src/routes/Login.tsx
+++ frontend/src/routes/Login.tsx
@@ -1,53 +1,127 @@
-import { FC } from 'react';
-
+import { FC, useContext, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {
+  LoginUserProps,
+  RegisterUserProps,
+} from '../components/authentication/auth.types';
+import { authContext } from '../context/authContext';
+import http from '../services/api';
 interface LoginProps {}
 
 const Login: FC<LoginProps> = ({}) => {
+  const navigate = useNavigate();
+  const registerName = useRef<HTMLInputElement>(null);
+  const registerEmail = useRef<HTMLInputElement>(null);
+  const registerPassword = useRef<HTMLInputElement>(null);
+  const registerVerifyPassword = useRef<HTMLInputElement>(null);
+  const loginEmail = useRef<HTMLInputElement>(null);
+  const loginPassword = useRef<HTMLInputElement>(null);
+  const context = useContext(authContext);
+
+  const textfieldStyleString =
+    'bg-transparent border-solid border-2 border-lightgrey-8008 rounded p-2';
+
+  const registerUserHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    const params: RegisterUserProps = {
+      name: registerName.current!.value!,
+      email: registerEmail.current!.value!,
+      password: registerPassword.current!.value!,
+      password2: registerVerifyPassword.current!.value!,
+    };
+    console.log(params);
+    http()
+      .post('register', params)
+      .then((res) => {
+        console.log(res);
+        alert('Please log in.');
+        loginEmail.current!.focus();
+      })
+      .catch((e) =>
+        alert(
+          e.response.data.errors.reduce(
+            (res: any, str: any) => res + '- ' + str.message + '\n',
+            '',
+            '',
+          ),
+        ),
+      );
+  };
+
+  const loginUserHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    const params: LoginUserProps = {
+      email: loginEmail.current!.value!,
+      password: loginPassword.current!.value!,
+    };
+    http()
+      .post('login', params)
+      .then((res) => {
+        console.log(res);
+        context?.setUser(res.data.name);
+        navigate('/');
+      })
+      .catch((e) =>
+        alert(
+          e.response.data.errors.reduce(
+            (res: any, str: any) => res + '- ' + str.message + '\n',
+            '',
+            '',
+          ),
+        ),
+      );
+  };
+
   return (
     <div className="flex justify-center items-center gap-[16rem]">
-      <div className="flex flex-col gap-3">
+      <form className="flex flex-col gap-3" onSubmit={registerUserHandler}>
         <h1>register</h1>
         <input
-          className="bg-transparent border-solid border-2 border-lightgrey-8008 rounded p-2"
+          className={textfieldStyleString}
           type="text"
           placeholder="username"
+          ref={registerName}
         />
         <input
-          className="bg-transparent border-solid border-2 border-lightgrey-8008 rounded p-2"
+          className={textfieldStyleString}
           type="email"
           placeholder="email"
+          ref={registerEmail}
         />
         <input
-          className="bg-transparent border-solid border-2 border-lightgrey-8008 rounded p-2"
-          type="email"
-          placeholder="verify email"
-        />
-        <input
-          className="bg-transparent border-solid border-2 border-lightgrey-8008 rounded p-2"
+          className={textfieldStyleString}
           type="password"
           placeholder="password"
+          ref={registerPassword}
         />
         <input
-          className="bg-transparent border-solid border-2 border-lightgrey-8008 rounded p-2"
+          className={textfieldStyleString}
           type="password"
           placeholder="verify password"
+          ref={registerVerifyPassword}
         />
-        <button>sign up</button>
-      </div>
-      <div className="flex flex-col gap-3">
+        <button type="submit" className="hover:bg-slate-100 transition p-1">
+          sign up
+        </button>
+      </form>
+      <form className="flex flex-col gap-4" onSubmit={loginUserHandler}>
         <h1>login</h1>
         <input
-          className="bg-transparent border-solid border-2 border-lightgrey-8008 rounded p-2"
+          className={textfieldStyleString}
           type="email"
           placeholder="email"
+          ref={loginEmail}
         />
         <input
-          className="bg-transparent border-solid border-2 border-lightgrey-8008 rounded p-2"
+          className={textfieldStyleString}
           type="password"
           placeholder="password"
+          ref={loginPassword}
         />
-        <button>log in</button>
-      </div>
+        <button type="submit" className="hover:bg-slate-100 transition p-1">
+          log in
+        </button>
+      </form>
     </div>
   );
 };
