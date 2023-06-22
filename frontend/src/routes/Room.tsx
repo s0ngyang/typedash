1632b07111dd99ed373096c087d1029b625b88a3
raynertjx <rayner742@gmail.com>
2023-06-23 01:13:53 +0800

    Added showChallenge emitting and receiving

--- frontend/src/routes/Room.tsx
+++ frontend/src/routes/Room.tsx
@@ -1,22 +1,32 @@
 import { FC, memo, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import TypingTest from '../components/typing/TypingTest';
+import { ChallengeProps } from '../components/typing/challenges/Books.constants';
 import { randomChallenge } from '../helpers/randomChallenge';
 import socket from '../services/socket';
 
 interface RoomProps {}
 
 const Room: FC<RoomProps> = ({}) => {
-  const chosenChallenge = randomChallenge();
-  const [roomId, setRoomId] = useState<string>();
+  const location = useLocation();
+  const [chosenChallenge, setChosenChallenge] = useState<ChallengeProps>();
+  const roomId = location.pathname.split('/')[2];
   const roomUrl = `https://typedash.com/multiplayer/${roomId}`;
-  console.log('rerender');
 
   socket.on('roomCreated', (arg) => {
     console.log('hi');
-    setRoomId(arg.id);
-    socket.emit('showChallenge', { challenge: chosenChallenge });
+    if (arg.id === socket.id) {
+      const random = randomChallenge();
+      setChosenChallenge(random);
+      socket.emit('showChallenge', { challenge: random });
+    }
   });
 
+  if (roomId !== socket.id) {
+    socket.on('showChallenge', (arg) => {
+      setChosenChallenge(arg.challenge);
+    });
+  }
   return (
     <>
       <TypingTest isMultiplayer={true} specificChallenge={chosenChallenge} />
