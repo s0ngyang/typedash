7fd17dc847d174fb1fff37b7925f79af84addfd0
raynertjx <rayner742@gmail.com>
2023-06-23 00:27:03 +0800

    Emit showChallenge event

--- frontend/src/routes/Room.tsx
+++ frontend/src/routes/Room.tsx
@@ -1,9 +1,28 @@
-import { FC } from 'react';
+import { FC, memo, useState } from 'react';
+import TypingTest from '../components/typing/TypingTest';
+import { randomChallenge } from '../helpers/randomChallenge';
+import socket from '../services/socket';
 
 interface RoomProps {}
 
 const Room: FC<RoomProps> = ({}) => {
-  return <div>Room</div>;
+  const chosenChallenge = randomChallenge();
+  const [roomId, setRoomId] = useState<string>();
+  const roomUrl = `https://typedash.com/multiplayer/${roomId}`;
+  console.log('rerender');
+
+  socket.on('roomCreated', (arg) => {
+    console.log('hi');
+    setRoomId(arg.id);
+    socket.emit('showChallenge', { challenge: chosenChallenge });
+  });
+
+  return (
+    <>
+      <TypingTest isMultiplayer={true} specificChallenge={chosenChallenge} />
+      <div>{roomUrl}</div>
+    </>
+  );
 };
 
-export default Room;
+export default memo(Room);
