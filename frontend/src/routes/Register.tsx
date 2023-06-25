f6321a3d53080982db5b0c5123024c8aa76787ca
raynertjx <rayner742@gmail.com>
2023-06-25 21:17:48 +0800

    Added toast notifications and integrated formik error messages

--- frontend/src/routes/Register.tsx
+++ frontend/src/routes/Register.tsx
@@ -1,3 +1,4 @@
+import { useToast } from '@chakra-ui/react';
 import { FormikValues, useFormik } from 'formik';
 import { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -7,11 +8,22 @@ import http from '../services/api';
 interface RegisterProps {}
 
 const Register: FC<RegisterProps> = ({}) => {
+  const toast = useToast();
   const navigate = useNavigate();
   const registerUserHandler = (values: FormikValues) => {
+    console.log(values);
     http()
       .post('register', values)
       .then(() => {
+        toast({
+          title: 'Registered successfully.',
+          description: 'Your account has been created.',
+          variant: 'subtle',
+          status: 'success',
+          position: 'top-right',
+          duration: 5000,
+          isClosable: true,
+        });
         navigate('/login');
       })
       .catch((e) =>
@@ -24,8 +36,6 @@ const Register: FC<RegisterProps> = ({}) => {
         ),
       );
   };
-  const textfieldStyleString =
-    'bg-transparent border-solid border-2 border-lightgrey-8008 rounded p-2';
 
   const getCharacterValidationError = (str: string) => {
     return `Your password must have at least 1 ${str} character.`;
@@ -33,14 +43,15 @@ const Register: FC<RegisterProps> = ({}) => {
 
   const formik = useFormik({
     initialValues: {
-      username: '',
+      name: '',
       email: '',
       password: '',
       password2: '',
     },
     validationSchema: Yup.object({
-      username: Yup.string()
-        .max(15, 'Must be 15 characters or less')
+      name: Yup.string()
+        .min(6, 'Username must be at least 6 characters')
+        .max(15, 'Username must be 15 characters or less')
         .required('Username is required'),
       email: Yup.string()
         .email('Invalid email address')
@@ -58,61 +69,100 @@ const Register: FC<RegisterProps> = ({}) => {
     onSubmit: registerUserHandler,
   });
   return (
-    <div className="flex justify-center items-center gap-[16rem]">
-      <form className="flex flex-col gap-3" onSubmit={formik.handleSubmit}>
+    <div className="flex flex-col justify-center items-center gap-6">
+      <form
+        className="flex flex-col gap-2 w-1/2 text-left"
+        onSubmit={formik.handleSubmit}
+      >
         <h1>register</h1>
-        <input
-          id="username"
-          name="username"
-          className={textfieldStyleString}
-          type="text"
-          placeholder="username"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.username}
-        />
-        {formik.touched.username && formik.errors.username ? (
-          <div>{formik.errors.username}</div>
-        ) : null}
-        <input
-          id="email"
-          name="email"
-          className={textfieldStyleString}
-          type="email"
-          placeholder="email"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.email}
-        />
-        {formik.touched.email && formik.errors.email ? (
-          <div>{formik.errors.email}</div>
-        ) : null}
-        <input
-          id="password"
-          name="password"
-          className={textfieldStyleString}
-          type="password"
-          placeholder="password"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.password}
-        />
-        {formik.touched.password && formik.errors.password ? (
-          <div>{formik.errors.password}</div>
-        ) : null}
-        <input
-          id="password2"
-          name="password2"
-          className={textfieldStyleString}
-          type="password"
-          placeholder="verify password"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.password2}
-        />
-        {formik.touched.password2 && formik.errors.password2 ? (
-          <div>{formik.errors.password2}</div>
-        ) : null}
+        <div className="flex flex-col gap-2">
+          <input
+            id="name"
+            name="name"
+            className={`bg-transparent border-solid border-2 rounded p-2 ${
+              formik.touched.name && formik.errors.name
+                ? 'border-red-600'
+                : 'border-lightgrey-8008'
+            }`}
+            type="text"
+            placeholder="username"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.name}
+          />
+          {formik.touched.name && formik.errors.name ? (
+            <div className="text-red-600 font-bold">{formik.errors.name}</div>
+          ) : (
+            <div>&nbsp;</div>
+          )}
+        </div>
+        <div className="flex flex-col gap-2">
+          <input
+            id="email"
+            name="email"
+            className={`bg-transparent border-solid border-2 rounded p-2 ${
+              formik.touched.email && formik.errors.email
+                ? 'border-red-600'
+                : 'border-lightgrey-8008'
+            }`}
+            type="email"
+            placeholder="email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <div className="text-red-600 font-bold">{formik.errors.email}</div>
+          ) : (
+            <div>&nbsp;</div>
+          )}
+        </div>
+        <div className="flex flex-col gap-2">
+          <input
+            id="password"
+            name="password"
+            className={`bg-transparent border-solid border-2 rounded p-2 ${
+              formik.touched.password && formik.errors.password
+                ? 'border-red-600'
+                : 'border-lightgrey-8008'
+            }`}
+            type="password"
+            placeholder="password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
+          />
+          {formik.touched.password && formik.errors.password ? (
+            <div className="text-red-600 font-bold">
+              {formik.errors.password}
+            </div>
+          ) : (
+            <div>&nbsp;</div>
+          )}
+        </div>
+        <div className="flex flex-col gap-2">
+          <input
+            id="password2"
+            name="password2"
+            className={`bg-transparent border-solid border-2 rounded p-2 ${
+              formik.touched.password2 && formik.errors.password2
+                ? 'border-red-600'
+                : 'border-lightgrey-8008'
+            }`}
+            type="password"
+            placeholder="verify password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password2}
+          />
+          {formik.touched.password2 && formik.errors.password2 ? (
+            <div className="text-red-600 font-bold">
+              {formik.errors.password2}
+            </div>
+          ) : (
+            <div>&nbsp;</div>
+          )}
+        </div>
         <button type="submit" className="hover:bg-slate-100 transition p-1">
           sign up
         </button>
