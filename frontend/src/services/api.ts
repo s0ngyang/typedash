945d6f70b8b1a05237e60636cc82a387b557d69e
raynertjx <rayner742@gmail.com>
2023-06-26 21:42:17 +0800

    Trying DO deploy

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -4,8 +4,9 @@ import.meta.env.MODE;
 const http = () => {
   const api_url: string =
     process.env.NODE_ENV === 'production'
-      ? 'https://typedash-api.onrender.com'
-      : 'http://localhost:3000/';
+      ? 'https://typedash-api-5bqlc.ondigitalocean.app/'
+      : 'http://localhost:3000';
+
   const instance = axios.create({
     // withCredentials: true,
     baseURL: api_url,
