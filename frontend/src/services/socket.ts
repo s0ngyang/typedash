7384b0401b96571589364bc4f5be879d8af43788
Song Yang <keesongyang4@gmail.com>
2023-06-28 13:39:11 +0800

    Fix return for updateloadout

--- frontend/src/services/socket.ts
+++ frontend/src/services/socket.ts
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client';
 const api_url =
   process.env.NODE_ENV === 'production'
     ? 'https://typedash-api-5bqlc.ondigitalocean.app/'
-    : 'http://localhost:3001/';
+    : 'http://localhost:3000/';
 
 const socket = io(api_url, {});
 
