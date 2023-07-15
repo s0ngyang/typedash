0465f1266e6a38bc1e1c78206c8cd69aa6c9b73d
raynertjx <rayner742@gmail.com>
2023-07-15 17:06:11 +0800

    Changed api url

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,11 +2,11 @@ import axios from 'axios';
 import.meta.env.MODE;
 
 const http = () => {
-  // const api_url: string =
-  //   process.env.NODE_ENV === 'production'
-  //     ? 'https://typedash-api-5bqlc.ondigitalocean.app/'
-  //     : 'http://localhost:3000/';
-  const api_url = 'https://typedash-api-5bqlc.ondigitalocean.app/';
+  const api_url: string =
+    process.env.NODE_ENV === 'production'
+      ? 'https://typedash-api-5bqlc.ondigitalocean.app/'
+      : 'http://localhost:3000/';
+  // const api_url = 'https://typedash-api-5bqlc.ondigitalocean.app/';
   // const api_url = 'http://localhost:3000/';
   const instance = axios.create({
     baseURL: api_url,
