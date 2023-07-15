930991265764300d7775411ac097adda8c834b4f
raynertjx <rayner742@gmail.com>
2023-07-15 18:09:05 +0800

    Removed unnecessary CRUD routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,15 +6,12 @@ import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
 import { authContext } from './context/authContext';
 import Account from './routes/Account';
-import CreateLoadout from './routes/CreateLoadout';
 import Layout from './routes/Layout';
-import Loadout from './routes/Loadout';
 import Login from './routes/Login';
 import Multiplayer from './routes/Multiplayer';
 import Register from './routes/Register';
 import Room from './routes/Room';
 import Singleplayer from './routes/Singleplayer';
-import UpdateLoadout from './routes/UpdateLoadout';
 
 function App() {
   const [user, setUser] = useState<string>();
@@ -43,20 +40,17 @@ function App() {
     <authContext.Provider value={{ user, setUser }}>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/singleplayer" element={<Singleplayer />} />
-          <Route path="/multiplayer" element={<Multiplayer />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/multiplayer">
+          <Route path='/singleplayer' element={<Singleplayer />} />
+          <Route path='/multiplayer' element={<Multiplayer />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/multiplayer'>
             <Route index={true} element={<Multiplayer />} />
-            <Route path=":roomId" element={<Room />} />
+            <Route path=':roomId' element={<Room />} />
           </Route>
-          <Route path="login" element={<Login />} />
-          <Route path="account" element={<Account />} />
-          <Route path="account/loadout" element={<Loadout />} />
-          <Route path="account/loadout/create" element={<CreateLoadout />} />
-          <Route path="account/loadout/update" element={<UpdateLoadout />} />
-          <Route path="*" element={<Navigate to="/singleplayer" replace />} />
+          <Route path='login' element={<Login />} />
+          <Route path='account' element={<Account />} />
+          <Route path='*' element={<Navigate to='/singleplayer' replace />} />
         </Route>
       </Routes>
     </authContext.Provider>
