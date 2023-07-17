5e0fb374b887a2e7e68ded75da05e7f1e4fee614
raynertjx <rayner742@gmail.com>
2023-07-17 23:16:11 +0800

    Added award badges and fixed styling

--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -1,6 +1,12 @@
 // @ts-nocheck
 import { Button, SlideFade, useToast } from '@chakra-ui/react';
 import { FC, useContext, useEffect, useState } from 'react';
+import {
+  TbRosetteNumber1,
+  TbRosetteNumber2,
+  TbRosetteNumber3,
+  TbRosetteNumber4,
+} from 'react-icons/tb';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { authContext } from '../context/authContext';
 import useTimer from '../helpers/useTimer';
@@ -31,6 +37,16 @@ const Room: FC<RoomProps> = ({}) => {
   const context = useContext(authContext);
   const username = context?.user || 'Guest';
   const toast = useToast();
+
+  const displayBadges = (position: number) => {
+    const badges = [
+      <TbRosetteNumber1 size={25} className="text-pink-8008" />,
+      <TbRosetteNumber2 size={25} className="text-pink-8008" />,
+      <TbRosetteNumber3 size={25} className="text-pink-8008" />,
+      <TbRosetteNumber4 size={25} className="text-pink-8008" />,
+    ];
+    return badges[position - 1];
+  };
 
   const leaveRoom = () => {
     socket.emit('leaveRoom');
@@ -97,21 +113,32 @@ const Room: FC<RoomProps> = ({}) => {
   }, [readyPlayers, numPlayers]);
 
   return (
-    <>
-      {listOfPlayers!.map((player) => (
-        <div key={player.id}>
-          <div>{player.username}</div>
-          <div>{rankings[player.id]}</div>
-          <div className="h-4 transition">
-            <SlideFade in={time === 0}>
-              <ProgressBar
-                lettersTyped={typingProgresses[player.id]}
-                totalLetters={chosenChallenge?.content.split('').length!}
-              />
-            </SlideFade>
-          </div>
+    <div className="flex flex-col justify-between">
+      <SlideFade in={time === 0}>
+        <div className="flex flex-col gap-4">
+          {listOfPlayers!.map((player) => (
+            <div
+              key={player.id}
+              className="flex items-center justify-between gap-6"
+            >
+              <div className="flex w-full items-center gap-6">
+                <div>{player.username}</div>
+                <div className="transition w-[95%]">
+                  <ProgressBar
+                    lettersTyped={typingProgresses[player.id]}
+                    totalLetters={chosenChallenge?.content.split('').length!}
+                  />
+                </div>
+              </div>
+              <div className="flex justify-end items-center w-8 h-8">
+                <SlideFade in={rankings[player.id]}>
+                  {displayBadges(rankings[player.id])}
+                </SlideFade>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
+      </SlideFade>
 
       <MultiplayerTest
         startTyping={time === 0}
@@ -136,7 +163,7 @@ const Room: FC<RoomProps> = ({}) => {
       </Button>
 
       <div>{roomUrl}</div>
-    </>
+    </div>
   );
 };
 
