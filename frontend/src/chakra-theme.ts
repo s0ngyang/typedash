dec221871b8e1b320c70198523403519f4554c0e
raynertjx <rayner742@gmail.com>
2023-07-23 23:33:11 +0800

    Added roboto mono font and adjusted caret offset to be fixed

--- frontend/src/chakra-theme.ts
+++ frontend/src/chakra-theme.ts
@@ -1,8 +1,12 @@
 import { extendTheme } from '@chakra-ui/react';
+import '@fontsource-variable/roboto-mono';
 
 export const customTheme = extendTheme({
   config: {
     useSystemColorMode: false,
+  },
+  fonts: {
+    body: `'Roboto Mono Variable', monospace`,
   },
   components: {
     Tooltip: {
