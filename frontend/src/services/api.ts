5666cc5d385977f9790f78278f1ffc53a8d5ae3a
s0ngyang <keesongyang4@gmail.com>
2023-09-27 00:23:21 +0800

    Update api url

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const http = () => {
   const api_url: string =
     process.env.NODE_ENV === 'production'
-      ? 'https://whale-app-s3qwf.ondigitalocean.app'
+      ? 'https://whale-app-s3qwf.ondigitalocean.app/'
       : 'http://localhost:3000/';
   const instance = axios.create({
     baseURL: api_url,
