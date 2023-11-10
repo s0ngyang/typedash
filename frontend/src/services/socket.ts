ffbe83a4357e4d2264104e5210e2a5151026e626
raynertjx <rayner742@gmail.com>
2023-11-10 15:56:16 +0800

    Change api url for socket to DO droplet

--- frontend/src/services/socket.ts
+++ frontend/src/services/socket.ts
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client';
 
 const api_url =
   process.env.NODE_ENV === 'production'
-    ? 'https://typedash-api-5bqlc.ondigitalocean.app/'
+    ? 'https://typedash-api.raynertoh.dev/'
     : 'http://localhost:3000/';
 
 const socket = io(api_url, {});
